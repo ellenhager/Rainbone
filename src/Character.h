8c45aad9b17431558e5dbd700aaad71e40a45138
Rasuttmus <rasmus270@hotmail.com>
2015-11-12 10:49:54 +0100

    added a base for a basic character, the transformation is what needs to be completed, then call the initialize and render functions from main

---
+++ src/Character.h
@@ -0,0 +1,64 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <iostream>
+#include <math.h>
+#include <sgct.h>
+#include <glm/gtc/matrix_inverse.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "objloader.hpp"
+
+class Character {
+
+public:
+
+    Character(glm::vec3, const char *, glm::vec4);
+    ~Character();
+
+    void initialize(glm::vec3);
+    void render(std::vector<glm::mat4>);
+
+private:
+
+    glm::vec3 convertToCartesian();
+    glm::vec3 convertToSpherical();
+
+    //glm::mat4 mTransform;
+
+    // Position is in spherical coordinates, i.e. (radius, theta, phi)
+    glm::vec3 mPosition;
+
+    // Lists containing vertices and normals
+    std::vector<glm::vec3> mVerts;
+    std::vector<glm::vec3> mNormals;
+
+    // Shader data
+    GLuint vertexArray;
+    GLuint vertexBuffer;
+    GLuint normalBuffer;
+
+    GLint MVPLoc;           // MVP matrix
+    GLint MVLoc;            // MV matrix
+    GLint MVLightLoc;       // MVLight matrix
+    GLint NMLoc;            // NM matrix
+    GLint lightPosLoc;      // Light position
+    GLint colorLoc;         // Color
+    GLint lightAmbLoc;      // Ambient light
+    GLint lightDifLoc;      // Diffuse light
+    GLint lightSpeLoc;      // Specular light
+    GLint specularityLoc;   // Specular constant
+    GLint shinynessLoc;     // How much specularity (magnitude)
+
+    struct Material {
+        glm::vec4 color;
+        glm::vec4 ambient;
+        glm::vec4 diffuse;
+        glm::vec4 specular;
+        float specularity;
+        float shinyness;
+    } mMaterial;
+
+};
+
+#endif // CHARACTER_H
